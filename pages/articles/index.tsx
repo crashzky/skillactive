1f2bad5ccebf6a065b94e125889e03bdea93ff39
Kirill Osintsev <osintsev.2000@inbox.ru>
2022-02-10 01:41:07 +0500

    hotfixes for deploy

--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -1,15 +1,12 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import useFeed from '../../hooks/useFeed';
 
 const ArticlesPage = (): JSX.Element => {
-	const setSelectedMenuItem = useFeed((state) => state.setSelectedMenuItem);
 	const router = useRouter();
 
 	useEffect(() => {
-		setSelectedMenuItem(2);
 		router.push('/feed');
-	}, []);
+	}, [router]);
 
 	return <></>;
 };
